4653869d524eeecde3e6243f220a980df2cbee6d
phrancism <francishmariano@gmail.com>
2020-03-22 16:20:09 -0700

    Scratch pad word on view controller

--- inimitablecities/inimitablecities/Controllers/ViewController.swift
+++ inimitablecities/inimitablecities/Controllers/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  inimitablecities
-//
-//  Created by Francis Mariano on 3/21/20.
-//  Copyright © 2020 Francis Mariano. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,8 +5,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let cityUniqueWords = CitiesUniqueWords.default.data.first { $0.name == "zirma" }!
+        let cityAllWords = CitiesAllWords.default.data.first { $0.name == "zirma" }!
+
+        let lettersOfUniqueWords = cityUniqueWords.uniqueWords.flatMap { $0.map(String.init) }
+
+        var counterLettersOfUniqueWords = ItalianAlphabetCounter()
+        lettersOfUniqueWords.forEach { letter in
+            counterLettersOfUniqueWords.counter[letter]! += 1
+        }
+        let counterSorted = counterLettersOfUniqueWords.counter.sorted { $0.key < $1.key }
+        print(counterSorted)
+
+        print(counterLettersOfUniqueWords.counter)
+        print(counterLettersOfUniqueWords.total)
+        print(counterLettersOfUniqueWords.relativeFrequencies)
     }
 
 
 }
-
